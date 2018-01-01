62704a28b24386aa8278617223f82e1b1b1cfa5a
Szakszim <saxim826@gmail.com>
2018-01-02 00:54:38 +0100

    Jakoś tak to powinno być

--- ChaOS v3.0/ChaOS v3.0/Headers/Siec.h
+++ ChaOS v3.0/ChaOS v3.0/Headers/Siec.h
@@ -1,11 +1,9 @@
 #ifndef SIEC__H
 #define SIEC__H
-//#include "..\Headers\ConditionVariable.h"
 #include"..\Headers\SMS.h"
 #include<memory>
 class Siec
 {
-	//ConditionVariable zmienna;
 public:
 	bool wyslij(std::string, int ID);
 	std::unique_ptr<SMS> odbierz();
