0f2733ca2b2c40f108328d9c1ff82a52b98ca7dc
Albert Millert <devmood.business@gmail.com>
2018-01-10 18:03:15 +0100

    chyba działa SRT i liczenie burst time

--- ChaOS v3.0/ChaOS v3.0/Sources/ProcessScheduler.cpp
+++ ChaOS v3.0/ChaOS v3.0/Sources/ProcessScheduler.cpp
@@ -8,7 +8,7 @@ extern Interpreter* i;
 
 void ProcessScheduler::RunProcess(int count)
 {
-	for (int i = 0; i < count; i++) 
+	for (int i = 0; i < count; i++)
 	{
 		RunProcess();
 	}
@@ -16,7 +16,6 @@ void ProcessScheduler::RunProcess(int count)
 
 void ProcessScheduler::RunProcess()
 {
-
 	if (ActiveProcess == nullptr)
 	{
 		SRTSchedulingAlgorithm();
@@ -31,8 +30,6 @@ void ProcessScheduler::RunProcess()
 			{
 				pm->killProcess(ActiveProcess->GetPID());
 				ActiveProcess = nullptr;
-				//Zerowanie zmiennych pomocniczych do liczenia wykonanych instrukcji ActiveProcess
-				startCounter = endCounter = differenceCounter = 0;
 				instructions = 0;
 
 				//zabezpieczenie przed nullptr; zanim cos sie stanie - ustawiany jest proces bezczynnosci
@@ -50,145 +47,73 @@ void ProcessScheduler::RunProcess()
 
 		else
 		{
-			//Sprawdzenie, czy ActiveProcess jest ustawiony na jakiœ proces
-			if (ActiveProcess != nullptr)
-			{
-				if (ActiveProcess->GetPID() != 1)
-				{
-					++instructions;
-				}
-				//Sprawdzenie rozmiaru listy readyProceesses -- size() > 1 oznacza, ¿e ActiveProcess nie jest procesem bezczynnoœci
-				if (pm->readyProcesses.size() > 1) {
-					//Wyliczenie, ile instrukcji wykona³o siê dla ActiveProcess
-					/*endCounter = ActiveProcess->GetInstructionCounter();
-					differenceCounter = endCounter - startCounter;*/
-					//ActiveProcess->SetProcesBurstTime(ActiveProcess->GetProcesBurstTime() - 1);
-				}
-				//Proces bezczynnoœci nie wykonuje instrukcji, wiêc nie ma powodu do ustawiania zmiennych pomocniczych
-			}
-
 			//Planista wywo³ywany za ka¿dym razem - obs³uguje wszystkie mo¿liwe przypadki
 			SRTSchedulingAlgorithm();
 		}
 	}
+
 	//Uruchomienie metody DoCommand() interpretera, gdzie mam nadzieje zmieniany jest instructionCounter wykonywanego procesu
 	i->DoCommand();
+
+	if (ActiveProcess->GetPID() > 1) {
+		instructions++;
+		//std::cout << "Ten" << ActiveProcess->GetPID() << "\t" << instructions << std::endl;
+	}
 }
 
 void ProcessScheduler::SRTSchedulingAlgorithm()
 {
-	//Gdy rozmiar readyProcesses == 1 -- tym procesem jest proces bezczynnoœci
-	//Gdy ActiveProcess == nullptr -- trzeba ustawiæ ActiveProcess jedynym elementem w readyProcesses
-	if (pm->readyProcesses.size() == 1) //==nullptr proces bzeczynnosci jeszcze nie jest ustawiony   && ActiveProcess == nullptr
+	if (pm->readyProcesses.size() == 1) // == nullptr proces bzeczynnosci jeszcze nie jest ustawiony   && ActiveProcess == nullptr
 	{
 		ActiveProcess = *pm->readyProcesses.begin();
-		//Dla procesu bezczynnoœci nie trzeba ustawiaæ ¿adnych zmiennych pomocniczych, poniewa¿ nie wykonuje on ¿adnych instrukcji
+		instructions = 0;
 		return;
 	}
-
-	//Gdy rozmiar readyProcesses == 1 -- procesem jest ponownie proces bezczynnoœci
-	//Gdy Activeprocess != nullptr -- nie trzeba ustawiaæ ActiveProcess
-	//tu sie sypie po usunieci procesu po jego wykonaniu
-	//if (pm->readyProcesses.size() == 1 && ActiveProcess->GetPID() == 1) //czyli ustawiony proces bezczynnosci
-	//{
-		//Nie trzeba te¿ niczego robiæ
-		//return;
-	//}
-
-
-
-	///
-	//Tylko, czy to kurka dobrze, ¿e siê ci¹gle wylicza nowy....? czy nie bedzie sie zmieniac o 1 ciagle?
-	//Zgodnie z tym, co mowi dr Bartoszek, trzeba liczyc czas przed porownaniem, ale tylko dla procesów innych niz bezczynnosci
-	//tu sypie sie...
-	//if (ActiveProcess != nullptr && ActiveProcess->GetPID() != 1)
-	//{
-	//	endCounter = ActiveProcess->GetInstructionCounter();
-	//	differenceCounter = endCounter - startCounter;
-	//	std::cout << "[~~ Albert ~~]\t\t" << ActiveProcess->GetPID() << "\t" << instructions << std::endl;
-	//	//std::cout << std::endl;
-	//	//std::cout << "[~~ Albert ~~]\t\t" << ActiveProcess->GetPID() << "\t" << differenceCounter << "\tinstrukcji" << std::endl;
-	//	//std::cout << "[~~ Albert ~~]\t\t" << ActiveProcess->GetPID() << "\t" << ActiveProcess->GetProcesBurstTime() << "\t burst time przed!" << std::endl;
-	//	ActiveProcess->SetProcesBurstTime(.5 * ActiveProcess->GetProcesBurstTime() + .5 * instructions);
-	//	//std::cout << "[~~ Albert ~~]\t\t" << ActiveProcess->GetPID() << "\t" << ActiveProcess->GetProcesBurstTime() << "\tburst time po!" << std::endl;
-	//	//std::cout << std::endl;
-	//}
-	///
-
-
-
+	if (ActiveProcess->GetPID() > 1 && ActiveProcess != nullptr) {
+		std::cout << "[~~ Albert ~~]\t\t" << ActiveProcess->GetPID() << "\t" << instructions << std::endl;
+		//std::cout << std::endl;
+		//std::cout << "[~~ Albert ~~]\t\t" << ActiveProcess->GetPID() << "\t" << differenceCounter << "\tinstrukcji" << std::endl;
+		std::cout << "[~~ Albert ~~]\t\t" << ActiveProcess->GetPID() << "\t" << ActiveProcess->GetProcesBurstTime() << "\t burst time przed!" << std::endl;
+		ActiveProcess->SetProcesBurstTime(.5 * ActiveProcess->GetProcesBurstTime() + .5 * instructions);
+		std::cout << "[~~ Albert ~~]\t\t" << ActiveProcess->GetPID() << "\t" << ActiveProcess->GetProcesBurstTime() << "\tburst time po!" << std::endl;
+	}
 
 	//Ustawienie iteratora pomocniczego na proces o najkrotszym burstTime zgodnie z algorytmem SRT
 	iteratorToMinElement = std::min_element(pm->readyProcesses.begin(), pm->readyProcesses.end(), [](PCB* x, PCB* y) { return x->GetProcesBurstTime() < y->GetProcesBurstTime(); });
 
-	//nie wiem czy to sie kiedykolwiek wywola jak nie ma nullptra ustawianego
-	//Gdy ActiveProcess == nullptr -- nie trzeba liczyc czasu dla poprzednio aktywnego procesu, poniewa¿ proces zostal:
-	//1) wykonany		2)usuniet z powodu bledu		-- jeszcze jakieœ przypadki?
-	if (ActiveProcess == nullptr) //czy to sie wywola jesli usunalem ustawiania ActiveProcess na nullptr
+	if (ActiveProcess == nullptr)
 	{
 		//Ustawienie ActiveProcess najlepszym procesem zgodnie z SRT
 		ActiveProcess = *iteratorToMinElement;
-
-		//Ustawienie zmiennych pomocniczych do liczenia wykonanych instrukcji ActiveProcess
-		startCounter = ActiveProcess->GetInstructionCounter();
-		endCounter = differenceCounter = 0;
 		instructions = 0;
+		return;
 	}
 
-	//ActiveProcess != nullptr -- ActiveProcess by³ ustawiony przez jakiœ proces
+	//ActiveProcess != nullptr
 	else
 	{
-		///Zgodnie z tym, co mowi dr Bartoszek, trzeba liczyc czas przed porownaniem
-		///ActiveProcess->SetProcesBurstTime(.5 * ActiveProcess->GetProcesBurstTime() + .5 * differenceCounter);
-
-		//tu sie sypalo
-		if (ActiveProcess != nullptr && ActiveProcess->GetPID() != 1)
-		{
-			endCounter = ActiveProcess->GetInstructionCounter();
-			differenceCounter = endCounter - startCounter;
-			std::cout << "[~~ Albert ~~]\t\t" << ActiveProcess->GetPID() << "\t" << instructions << std::endl;
-			//std::cout << std::endl;
-			//std::cout << "[~~ Albert ~~]\t\t" << ActiveProcess->GetPID() << "\t" << differenceCounter << "\tinstrukcji" << std::endl;
-			//std::cout << "[~~ Albert ~~]\t\t" << ActiveProcess->GetPID() << "\t" << ActiveProcess->GetProcesBurstTime() << "\t burst time przed!" << std::endl;
-			ActiveProcess->SetProcesBurstTime(.5 * ActiveProcess->GetProcesBurstTime() + .5 * instructions);
-			//std::cout << "[~~ Albert ~~]\t\t" << ActiveProcess->GetPID() << "\t" << ActiveProcess->GetProcesBurstTime() << "\tburst time po!" << std::endl;
-			//std::cout << std::endl;
-		}
-
 		//Sprawdzenie przy pomoy PID, czy wybrany proces jest na pewno innym procesem
 		if ((*iteratorToMinElement)->GetPID() != ActiveProcess->GetPID())
 		{
 			//Sprawdzenie, czy wybrany proces, obecnie ustawiony w iteratorze iteratorToMinElement, ma przewidywany czas krótszy od ActiveProcess
 			if ((*iteratorToMinElement)->GetProcesBurstTime() < ActiveProcess->GetProcesBurstTime()) //nie wiem do konca, czy z tym differenceCounter, czy jescze nie /// - differenceCounter
 			{
-				//if and else should be together
-				//if (ActiveProcess->GetPID() != 1)
-				//{
-					//Ustawienie ActiveProcess
-					ActiveProcess = *iteratorToMinElement;
-					//Ustawienie zmiennych pomocniczych do liczenia wykonywanych instrukcji ActiveProcess
-					startCounter = ActiveProcess->GetInstructionCounter();
-					endCounter = differenceCounter = 0;
-					instructions = 0;
-				//}
-				//else {
-					//Ustawienie ActiveProcess
-							//ActiveProcess = *iteratorToMinElement;
-					//Ustawienie zmiennych pomocniczych do liczenia wykonywanych instrukcji ActiveProcess
-							//startCounter = ActiveProcess->GetInstructionCounter();
-							//endCounter = differenceCounter = 0;
-				//}
+				ActiveProcess = *iteratorToMinElement;
+				instructions = 0;
+				return;
 			}
-			else 
+			else
 			{
 				return;
 			}
 		}
+
 		//Sytuacja, w której zosta³ wybrany ten sam proces, co ActiveProcess
-		else 
+		else
 		{
 			return;
 		}
 	}
+
 
 }
