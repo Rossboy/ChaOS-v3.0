0dd009bfae1dc316c04164adf34226739cfd4bb7
Michał Mirończuk <michal4010@wp.pl>
2018-01-11 12:08:29 +0100

    Zbedne komentarze :P

--- ChaOS v3.0/ChaOS v3.0/Headers/ProcessesManager.h
+++ ChaOS v3.0/ChaOS v3.0/Headers/ProcessesManager.h
@@ -14,7 +14,7 @@ public:
 	std::list<PCB*>waitingProcesses{};
 	std::list<PCB*>list{};
 	std::list<PCB*>readyProcesses{};
-	ProcessesManager(); //Poprawilem bo bylo {}; i przez to byl problem ze konstruktor jest juz zdefiniowany - Bartek
+	ProcessesManager(); 
 	void createProcess(std::string fileName, int GID);
 	void createProcess(std::string fileName, int GID, int AddidtionalSpace);
 
@@ -23,10 +23,8 @@ public:
 	void displayWaitingProcesses();
 	void displayReadyProcesses();
 	PCB * findPCBbyPID(int PID);
-	// Albert -- napisalem se funkcje do zwracania std::list<Process*>readyProcesses;
 	std::list<PCB*> GiveReadyProcessesList();
 	std::list<PCB*> GiveWaitingProcessesList();
-	//Konrad: potrzebowalem tego heh
 	std::list<std::list<PCB*>> getAllProcesseslist();
 	void AddProcessToReady(PCB*);
 	void RemoveProcessFromReady(PCB*);
